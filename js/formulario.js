7cad80271c3926d00309fcc08de49aeedee9973a
jucasval <juanfrancastilla@gmail.com>
2020-08-10 10:00:30 +0200

    Validación de las contraseñas.
    Función para comprobar si las contraseñas coinciden.

--- js/formulario.js
+++ js/formulario.js
@@ -17,28 +17,26 @@ formulario.addEventListener('submit', function (e) {
     //evitamos que se ejecute el evento
 });
 
-const validarFormulario = function (e) {
-
-    //1º.- Trasladamos el código a una función. Para reutilizarla en todos los inputs. 
+const validarFormulario = function (e) {//1º. Validamos el resto de imputs, menos password2. 
     switch (e.target.name) {
         case "usuario":
             validarCampo(expresiones.usuario, e.target, 'usuario');
-            //3º - Pasamos por parámetros los tres valores que necesita la función
             break;
         case "nombre":
-
+            validarCampo(expresiones.nombre, e.target, 'nombre');
             break;
         case "password":
-
+            validarCampo(expresiones.password, e.target, 'password');
             break;
         case "password2":
-
+            //2º. Validamos password2. Creamos una función para ello.
+            validarPassword2();
             break;
         case "correo":
-
+            validarCampo(expresiones.correo, 'correo');
             break;
         case "telefono":
-
+            validarCampo(expresiones.telefono, e.target, 'telefono');
             break;
 
     }
@@ -49,14 +47,6 @@ inputs.forEach(function (input) {
     input.addEventListener('keyup', validarFormulario);
     input.addEventListener('blur', validarFormulario);
 });
-
-/*
-    2º.- A la función tenemos que pasarle 3 valores:
-        - La expresión a comparar.
-        - El input donde se va a realizar la validación.
-        - El campo. Ej. grupo__usuario, grupo__password, etc. Queremos que sea
-          dinámico. 
-*/
 
 const validarCampo = function (expresion, input, campo) {
 
@@ -78,6 +68,26 @@ const validarCampo = function (expresion, input, campo) {
         document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add("formulario__input-error-activo");
     }
 
-    //4º - Sustituimos las comillas simples o dobles por "`" (símbolo de la tilde) y
-    //     añadimos el "campo", ${campo}. 
+
+};
+
+const validarPassword2 = function () {
+    const inputPassword1 = document.getElementById("password");
+    const inputPassword2 = document.getElementById("password2");
+
+    if (inputPassword1.value !== inputPassword2.value) {
+        document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-correcto");
+        document.getElementById(`grupo__password2`).classList.add("formulario__grupo-incorrecto");
+        document.querySelector(`#grupo__password2 i`).classList.remove("fa-check-circle");
+        document.querySelector(`#grupo__password2 i`).classList.add("fa-times-circle");
+        //mostramos el mensaje de error
+        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add("formulario__input-error-activo");
+    } else {
+        document.getElementById(`grupo__password2`).classList.add("formulario__grupo-correcto");
+        document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-incorrecto");
+        document.querySelector(`#grupo__password2 i`).classList.add("fa-check-circle");
+        document.querySelector(`#grupo__password2 i`).classList.remove("fa-times-circle");
+        //mostramos el mensaje de error
+        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove("formulario__input-error-activo");
+    }
 }
